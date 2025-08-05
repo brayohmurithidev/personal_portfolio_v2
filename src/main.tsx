b7af263604a31bbb4e905733938cf36ea76a3c1e
brayohmurithidev <murithibrianm@gmail.com>
2025-08-05 18:03:08 +0300

    added helment-sync for ssr

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import {createRoot} from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import {ThemeProvider} from "@/components/theme-provider.tsx";
+import {HelmetProvider} from "react-helmet-async";
 
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </ThemeProvider>,
 );
